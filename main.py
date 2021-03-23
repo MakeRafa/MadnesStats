3ccfc087033a60c505811150d31b72545319d1b9
MakeRafa <rafavazquez229@gmail.com>
2021-03-23 00:47:47 -0700

    Creating home page and connecting routes

--- main.py
+++ main.py
@@ -6,14 +6,33 @@ from pprint import PrettyPrinter
 
 app = Flask(__name__)
 
-API_URL ='https://balldontlie.io/api/v1/players'
+pp = PrettyPrinter(indent=4)
 
-@app.route('/home')
+API_URL ='https://balldontlie.io/api/v1/players/237'
+
+@app.route('/')
 def home():
     """Display the logo and search bars"""
 
     return render_template('home.html')
 
+@app.route('/player_result')
+def player_result():
+    player_id = request.args.get('id')
+
+    params = {
+        'id': player_id
+    }
+    result_json = requests.get(API_URL, params=params).json()
+    pp.pprint(result_json)
+
+    context = {
+        'first_name': result_json[first_name],
+        'last_name': result_json[last_name],
+        'position': result_json[position]
+    }
+
+    return render_template('player.html')
 
 if __name__ == '__main__':
 
