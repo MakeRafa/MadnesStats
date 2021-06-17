4fa975959863c84f48df72ff5829d18058ff440d
MakeRafa <rafavazquez229@gmail.com>
2021-06-17 08:18:46 -0700

    refactored main function to get player info, next step is to convert rest of the main.py file to js

--- scripts.js
+++ scripts.js
@@ -6,6 +6,11 @@ request.open('GET', 'https://www.balldontlie.io/api/v1/players', true)
 
 // This is where the basketball player data will be accessed
 request.onload = () => {
+    var data = JSON.parse(this.response)
+    data.forEach((player) => {
+        console.log(player.first_name)
+
+    })
 
 }
 
